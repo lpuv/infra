4211bf493624e825dcd6c3c4f196153075316f50
Leo Puvilland <leo@craftcat.dev>
2023-08-17 09:37:21 -0700

    initial files

---
+++ vars.tf
@@ -0,0 +1,13 @@
+#Set your public SSH key here
+variable "ssh_key" {
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/5d3jAjWq1fOIyPk3jHMafTLJH6xqHv2bAZalQ/GaL leo@craftcat.dev"
+}
+variable "api_url" {
+    default = "https://192.168.0.25:8006/api2/json"
+}
+#Blank var for use by terraform.tfvars
+variable "token_secret" {
+}
+#Blank var for use by terraform.tfvars
+variable "token_id" {
+}
